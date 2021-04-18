909e23c25a1091b2fcb7612a3f0750bceb3d62eb
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-04-18 10:03:42 -0400

    Add heroku db link for config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://sheltered-sands-26530.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
