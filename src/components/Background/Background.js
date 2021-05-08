94a58e3827560e846b53d24c5d2a73cdf0f1dae1
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-05-07 21:31:24 -0400

    Add title page font

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -13,12 +13,22 @@ class Background extends Component {
       height: '100vh',
       // width: '100vw',
       color: 'White',
-      textAlign: 'center'
+      textAlign: 'Center',
+      textShadow: '-2px 0 black, 0 3px black, 1px 0 black, 0 -1px black'
     }
+
+    // const textStyle = {
+    //
+    //
+    // }
     return (
       <div style={backgroundStyle}>
-        <h3>Welcome to Not a Tourist</h3>
-        <h5>Sign in or Sign up to access message board</h5>
+        <div className='col-4 pt-5 ml-1 box'>
+          <h2 className=''>Welcome to </h2>
+          <h2 className='ml-5 pl-5'> Not a Tourist</h2>
+          <h5 className='mt-5'>Sign in or Sign up</h5>
+          <h5 className='ml-5 pl-5'>to access message board</h5>
+        </div>
       </div>
     )
   }
