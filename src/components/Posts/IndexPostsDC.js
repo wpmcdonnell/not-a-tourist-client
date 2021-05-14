4eb466b8b520a9ad3eec78642182d603c5dd77ed
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-05-14 14:39:48 -0400

    Add pictures to DC index

--- src/components/Posts/IndexPostsDC.js
+++ src/components/Posts/IndexPostsDC.js
@@ -28,7 +28,8 @@ class IndexPostsDC extends Component {
       // We'll be modifying the state after we get our data
       // initially we have no data & our state should show that
       posts: null,
-      create: false
+      create: false,
+      pictures: null
     }
   }
 
@@ -49,6 +50,23 @@ class IndexPostsDC extends Component {
         // Set the state to hold the array of posts
         // this will cause a re-render
         this.setState({ posts: response.data.posts.reverse() })
+      })
+      .catch(console.error)
+
+    axios({
+      url: `${apiUrl}/dc-posts-pictures`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+
+      .then(response => {
+        console.log(response.data)
+        // Set the state to hold the array of posts
+        // this will cause a re-render
+        this.setState({ pictures: response.data.pictures })
+        console.log(this.state.pictures)
       })
       .catch(console.error)
   }
@@ -79,20 +97,21 @@ class IndexPostsDC extends Component {
 
     if (create) {
       return <Redirect to={'/create-post-dc'}/>
-    } else if (!this.state.posts) {
+    } else if (!this.state.posts || !this.state.pictures) {
       // if the posts state is null
       postsJsx = <p>Loading...</p>
     } else if (this.state.posts.length === 0) {
       // if posts array has length of 0 it's empty
       postsJsx = <p>No posts! Go create some.</p>
-    } else {
+    } else if (this.state.posts && this.state.pictures) {
       // we have posts! display them
       postsJsx = (
         <div className='mb-1'>
-          {this.state.posts.map(post => (
-            <Card className='mb-2 shadow bg-white rounded'style={{ }} key={post._id}>
+          {this.state.posts.concat(this.state.pictures).slice().sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map(post => (
+            <Card className='mb-2 shadow bg-white rounded' style={{ }} key={post._id}>
               <Card.Body>
                 <Card.Title>
+                  <Card.Img className='mb-3' variant="top" src={post.url}/>
                   <Link to={`/dc-posts/${post._id}`}>{post.title}</Link>
                 </Card.Title>
                 <p className='post-index-date d-inline'>{moment(post.createdAt).startOf('hour').fromNow()} </p>
@@ -111,7 +130,7 @@ class IndexPostsDC extends Component {
             <Link className='text-black mb-3' to={'/cities/'}> <h5> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
               <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
             </svg> Cities </h5> </Link>
-            <h1 className='mb-1'>DC NATION</h1>
+            <h1 className='mb-1'>DC Chillin</h1>
             <Button className='mb-2 shadow' variant='primary' onClick={this.create}>Create a Post</Button>
             <h3 className='mb-3'>Check out all the sweet posts</h3>
             {postsJsx}
